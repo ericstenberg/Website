cd9698d78d3b10c653f1b2e3285f2d851edc5f2d
Eric Stenberg <eric.d.stenberg@gmail.com>
2020-10-16 01:57:29 -0400

    Separated content pages from index pages. Finished header HTML. Moving onto left column HTML.

---
+++ main/footer.js
@@ -0,0 +1,8 @@
+<script type="module">
+    <nav>
+        <strong>Home</strong>
+        <a href="about.html">About</a>
+        <a href="disclaimerandprivacypolicy.html">Disclaimer / Privacy Policy</a>
+    </nav>
+    <h6>This page is available under the GNU Affero General Public License v3.0.</h6>
+</script>
